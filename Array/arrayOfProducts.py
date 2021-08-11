2a01e38c5f5a66e46648ea389e98cb055e36f6aa
Ell-91 <cdsherman@ucdavis.edu>
2021-08-11 16:05:30 -0700

    finished implementation

---
+++ Array/arrayOfProducts.py
@@ -0,0 +1,20 @@
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+    
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+    
+    return products
+
+
+ 
+
+array = [5, 1, 4, 2]
+print(arrayOfProducts(array))
