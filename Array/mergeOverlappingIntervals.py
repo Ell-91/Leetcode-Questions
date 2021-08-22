eb33b09bfcb42c5b203625e208579811e970f0f5
Ell-91 <cdsherman@ucdavis.edu>
2021-08-21 22:20:34 -0700

    updated

--- Array/mergeOverlappingIntervals.py
+++ Array/mergeOverlappingIntervals.py
@@ -1,11 +1,24 @@
 def mergeOverlappingIntervals(intervals):
-    intervals.sort  #[[1,2], [3,5], [4,7], [6,8], [9,10]]
+    #check if the list is empty
+
+    #sort the intervals
+    intervals.sort()
+
+    #Initialize a variable to hold the merged intervals
     merged_intervals = []
+    #print(merged_intervals[-1][1])
 
+    #start times are indexed at 0 // end times are indexed at 1
+    
     for interval in intervals:
-        if 
-        print(intervals[-1][1])
+        if merged_intervals == [] or merged_intervals[-1][1] < interval[0]:
+            merged_intervals.append(interval) #add interval to results
+            
+        else:
+            merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])    
+    return print(merged_intervals)
+    #print(intervals[0])
 
 
-array = [[1,2], [3,5], [4,7], [6,8], [9,10]]
-print(mergeOverlappingIntervals(array))
+interval = [[1,3],[2,6],[15,18],[8,10]]
+mergeOverlappingIntervals(interval)
