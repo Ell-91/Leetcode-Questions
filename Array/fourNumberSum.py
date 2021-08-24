0f3795858be43375d482ab0499c841c84b4ad862
Ell-91 <cdsherman@ucdavis.edu>
2021-08-24 00:19:01 -0700

    lll

--- Array/fourNumberSum.py
+++ Array/fourNumberSum.py
@@ -1,15 +1,30 @@
 def fourNumberSum(array, targetSum):
+    #if array == [] or len(array) < 4:
+        #return False
+
     array.sort()
-    quadruplets = dict()
-    results = []
+    result = []
 
-    for i in range(len(array)):
-        for j in range(len(array)):
-            currentSum = array[i] + array[j]
-            complement = targetSum - currentSum
+    for i in range(len(array)-3):
+        for j in range(i + 1, len(array)-2):
+            left_ptr = j + 1
+            right_ptr = len(array) - 1
 
-            quadruplets[currentSum] = i
-    return quadruplets
+            currentSum = array[i] + array[j] + array[left_ptr] + array[right_ptr]
+
+            while left_ptr < right_ptr:
+                if currentSum == targetSum:
+                    result.append([array[i], array[j], array[left_ptr], array[right_ptr]])
+                    left_ptr += 1
+                    right_ptr -= 1
+                
+                elif currentSum > targetSum:
+                    right_ptr -= 1
+                
+                elif currentSum < targetSum:
+                    left_ptr += 1
+        return result
+             
 
 
 array = [7, 6, 4, -1, 1, 2] 
